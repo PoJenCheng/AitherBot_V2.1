8c6ab57ff3e7fb97b3ce1d066822a24f2fc1a5b7
Eric <ericcheng@brainnavi.com>
2023-06-07 14:55:27 +0800

    Robot subFunction update

---
+++ FunctionLib_Robot/_subFunction.py
@@ -0,0 +1,23 @@
+from FunctionLib_Robot.__init__ import *
+
+# 過濾掉高頻雜訊
+def lowPass(filterData):
+    ignorePoint = []
+    dataTemp = []
+    k = 1
+    for point in range(len(filterData)):
+        if point == 0 and filterData[point] != 0:
+            dataTemp.append(filterData[point])
+        else:
+            diff = abs(filterData[point] - filterData[point-k])
+            if diff <= filterTolerance*k:
+                dataTemp.append(filterData[point])
+                k = 1
+            elif diff > filterTolerance:
+                k += 1
+                ignorePoint.append(point)
+    
+    for i in range(len(ignorePoint)):
+        filterData[ignorePoint[i]] = 0
+    
+    return filterData
